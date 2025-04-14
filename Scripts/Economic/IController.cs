daabaec6087715633abadacce0bf5b6c0163211c
denis <denis@DESKTOP-I5LBTCE>
2025-04-14 13:42:58 +0300

    added main core elements

---
+++ Scripts/Economic/IController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Assets.Economic
+{
+	public abstract class IController : MonoBehaviour
+	{
+		public abstract int Order { get; }
+
+		public abstract bool Init();
+
+		public abstract void StateUpdate();
+	}
+}
---
+++ Scripts/Economic/Models/EconomicItem.cs
@@ -0,0 +1,9 @@
+/// <summary>Данные о ресурсе.</summary>
+public class EconomicItem 
+{
+	public string Name { get; set; }
+
+	public string Description { get; set; }
+
+	public long Value { get; set; }
+}
